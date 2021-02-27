2fc1fca4532932ae0f691c18dced0c572984c6eb
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2021-02-26 21:17:06 -0600

    Video 5_5

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -23,4 +23,16 @@ class PostControllerTest extends TestCase
 
         $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
     }
+
+    public function test_validate_title()
+    {
+
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
+            'title' => ''
+        ]);
+
+        // Estatus HTTP 422
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors('title');
+    }
 }
